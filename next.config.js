5f4c91d8ea345c0d067d5e6b54619c37c28a05e1
Evoney Tavares <evoney.tavares@gmail.com>
2023-12-27 20:18:28 -0400

    change output

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  experimental: {
+    appDir: true,
+  },
 }
- 
+
 module.exports = nextConfig
